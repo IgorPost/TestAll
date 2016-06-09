8f82aa4fc3984be8924112afee705f5ebc862fa5
IgorPost <igor_post@ukr.net>
2016-06-09 17:53:08 +0300

    WorkPC

--- app/src/main/java/com/example/inalivayko/testall/examples_library/date_calendar/ActivityDateCalendar.java
+++ app/src/main/java/com/example/inalivayko/testall/examples_library/date_calendar/ActivityDateCalendar.java
@@ -1,19 +1,17 @@
-package com.example.inalivayko.testall.examples_library;
+package com.example.inalivayko.testall.examples_library.date_calendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.inalivayko.testall.R;
 
-public class ActivityExamplesLibrary extends AppCompatActivity {
+public class ActivityDateCalendar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_examples_library);
+        setContentView(R.layout.activity_activity_date_calendar);
 
 
     }
-
-
 }
